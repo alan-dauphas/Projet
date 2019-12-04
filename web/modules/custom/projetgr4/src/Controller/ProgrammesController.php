9438183d9adc0f01ec9369a8b33d976b8ca34fd3
riaze <ria.thas20@gmail.com>
2019-12-04 12:35:49 +0100

    first commit from riaze

--- web/modules/custom/projetgr4/src/Controller/ProgrammesController.php
+++ web/modules/custom/projetgr4/src/Controller/ProgrammesController.php
@@ -4,14 +4,14 @@ namespace Drupal\projetgr4\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Console\Bootstrap\Drupal;
-use \Drupal\user\UserInterface;
+use Drupal\user\UserInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 
 
-class HelloController extends ControllerBase{
+class ProgrammesController extends ControllerBase{
 
-    public function content(\Drupal\user\UserInterface $user)
+    public function content(UserInterface $user)
     {
         
         $msg = $this->t('you are name @username', ['@username'=> $this->currentUser()->getDisplayName(),]);
