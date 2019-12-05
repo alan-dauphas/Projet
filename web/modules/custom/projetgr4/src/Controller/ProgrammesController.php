f5928932e134f894cf674b709866ab49324e1d6e
alan-dauphas <alan.dauphas@me.com>
2019-12-05 16:45:46 +0100

    add link task participants, and add custom theme mama not activated

--- web/modules/custom/projetgr4/src/Controller/ProgrammesController.php
+++ web/modules/custom/projetgr4/src/Controller/ProgrammesController.php
@@ -7,15 +7,22 @@ use Drupal\Console\Bootstrap\Drupal;
 use Drupal\user\UserInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class ProgrammesController extends ControllerBase{
 
     public function content(UserInterface $user)
     {
-        
+
         $msg = $this->t('you are name @username', ['@username'=> $this->currentUser()->getDisplayName(),]);
         return ['#markup' => $msg];
-    
+
+    }
+
+    public function participants()
+    {
+      $response = new Response('blabla');
+      return $response;
     }
 }
