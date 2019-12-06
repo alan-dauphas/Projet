3ed9fc7d344f70e79780a726f1c26a6a778b5e74
alan-dauphas <alan.dauphas@me.com>
2019-12-06 12:02:07 +0100

    theme compatibility update...

--- web/themes/custom/mama/theme-settings.php
+++ web/themes/custom/mama/theme-settings.php
@@ -9,120 +9,120 @@
  *   A keyed array containing the current state of the form.
  */
 
-function nexus_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-  $form['nexus_settings'] = array(
+function mama_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  $form['mama_settings'] = array(
     '#type' => 'fieldset',
-    '#title' => t('Nexus Settings'),
+    '#title' => t('mama Settings'),
     '#collapsible' => FALSE,
     '#collapsed' => FALSE,
   );
-  $form['nexus_settings']['show_breadcrumbs'] = array(
+  $form['mama_settings']['show_breadcrumbs'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show breadcrumbs in a page'),
-    '#default_value' => theme_get_setting('show_breadcrumbs','nexus'),
+    '#default_value' => theme_get_setting('show_breadcrumbs','mama'),
     '#description'   => t("Check this option to show breadcrumbs in page. Uncheck to hide."),
   );
-  $form['nexus_settings']['slideshow'] = array(
+  $form['mama_settings']['slideshow'] = array(
     '#type' => 'fieldset',
     '#title' => t('Front Page Slideshow'),
     '#collapsible' => TRUE,
     '#collapsed' => FALSE,
   );
-  $form['nexus_settings']['slideshow']['slideshow_display'] = array(
+  $form['mama_settings']['slideshow']['slideshow_display'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show slideshow'),
-    '#default_value' => theme_get_setting('slideshow_display','nexus'),
+    '#default_value' => theme_get_setting('slideshow_display','mama'),
     '#description'   => t("Check this option to show Slideshow in front page. Uncheck to hide."),
   );
-  $form['nexus_settings']['slideshow']['slide'] = array(
+  $form['mama_settings']['slideshow']['slide'] = array(
     '#markup' => t('You can change the description and URL of each slide in the following Slide Setting fieldsets.'),
   );
-  $form['nexus_settings']['slideshow']['slide1'] = array(
+  $form['mama_settings']['slideshow']['slide1'] = array(
     '#type' => 'fieldset',
     '#title' => t('Slide 1'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
-  $form['nexus_settings']['slideshow']['slide1']['slide1_head'] = array(
+  $form['mama_settings']['slideshow']['slide1']['slide1_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide1_head','nexus'),
+    '#default_value' => theme_get_setting('slide1_head','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide1']['slide1_desc'] = array(
+  $form['mama_settings']['slideshow']['slide1']['slide1_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide1_desc','nexus'),
+    '#default_value' => theme_get_setting('slide1_desc','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide1']['slide1_url'] = array(
+  $form['mama_settings']['slideshow']['slide1']['slide1_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide1_url','nexus'),
+    '#default_value' => theme_get_setting('slide1_url','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide1']['slide1_image'] = array(
+  $form['mama_settings']['slideshow']['slide1']['slide1_image'] = array(
     '#type' => 'managed_file',
     '#title' => t('Image 1'),
-    '#default_value' => theme_get_setting('slide1_image','nexus'),
+    '#default_value' => theme_get_setting('slide1_image','mama'),
     '#upload_location' => 'public://',
   );
 
-  $form['nexus_settings']['slideshow']['slide2'] = array(
+  $form['mama_settings']['slideshow']['slide2'] = array(
     '#type' => 'fieldset',
     '#title' => t('Slide 2'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
-  $form['nexus_settings']['slideshow']['slide2']['slide2_head'] = array(
+  $form['mama_settings']['slideshow']['slide2']['slide2_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide2_head','nexus'),
+    '#default_value' => theme_get_setting('slide2_head','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide2']['slide2_desc'] = array(
+  $form['mama_settings']['slideshow']['slide2']['slide2_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide2_desc','nexus'),
+    '#default_value' => theme_get_setting('slide2_desc','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide2']['slide2_url'] = array(
+  $form['mama_settings']['slideshow']['slide2']['slide2_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide2_url','nexus'),
+    '#default_value' => theme_get_setting('slide2_url','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide2']['slide2_image'] = array(
+  $form['mama_settings']['slideshow']['slide2']['slide2_image'] = array(
     '#type' => 'managed_file',
     '#title' => t('Image 2'),
-    '#default_value' => theme_get_setting('slide2_image','nexus'),
+    '#default_value' => theme_get_setting('slide2_image','mama'),
     '#upload_location' => 'public://',
   );
-  $form['nexus_settings']['slideshow']['slide3'] = array(
+  $form['mama_settings']['slideshow']['slide3'] = array(
     '#type' => 'fieldset',
     '#title' => t('Slide 3'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
-  $form['nexus_settings']['slideshow']['slide3']['slide3_head'] = array(
+  $form['mama_settings']['slideshow']['slide3']['slide3_head'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide Headline'),
-    '#default_value' => theme_get_setting('slide3_head','nexus'),
+    '#default_value' => theme_get_setting('slide3_head','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide3']['slide3_desc'] = array(
+  $form['mama_settings']['slideshow']['slide3']['slide3_desc'] = array(
     '#type' => 'textarea',
     '#title' => t('Slide Description'),
-    '#default_value' => theme_get_setting('slide3_desc','nexus'),
+    '#default_value' => theme_get_setting('slide3_desc','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide3']['slide3_url'] = array(
+  $form['mama_settings']['slideshow']['slide3']['slide3_url'] = array(
     '#type' => 'textfield',
     '#title' => t('Slide URL'),
-    '#default_value' => theme_get_setting('slide3_url','nexus'),
+    '#default_value' => theme_get_setting('slide3_url','mama'),
   );
-  $form['nexus_settings']['slideshow']['slide3']['slide3_image'] = array(
+  $form['mama_settings']['slideshow']['slide3']['slide3_image'] = array(
     '#type' => 'managed_file',
     '#title' => t('Image 3'),
-    '#default_value' => theme_get_setting('slide3_image','nexus'),
+    '#default_value' => theme_get_setting('slide3_image','mama'),
     '#upload_location' => 'public://',
   );
-  $form['nexus_settings']['slideshow']['slideimage'] = array(
+  $form['mama_settings']['slideshow']['slideimage'] = array(
     '#markup' => t('To change the default Slide Images, Replace the slide-image-1.jpg, slide-image-2.jpg and slide-image-3.jpg in the images folder of the theme folder.'),
   );
-  $form['#submit'][] = 'nexus_settings_form_submit';
+  $form['#submit'][] = 'mama_settings_form_submit';
   $theme = \Drupal::theme()->getActiveTheme()->getName();
   $theme_file = drupal_get_path('theme', $theme) . '/theme-settings.php';
   $build_info = $form_state->getBuildInfo();
@@ -132,7 +132,7 @@ function nexus_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormSt
   $form_state->setBuildInfo($build_info);
 }
 
-function nexus_settings_form_submit(&$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+function mama_settings_form_submit(&$form, \Drupal\Core\Form\FormStateInterface $form_state) {
   $account = \Drupal::currentUser();
   $values = $form_state->getValues();
   for ($i = 1; $i <= 3; $i++) {
