857aa1e1212a1828b5c56b96222e743547cc26bb
riaze <ria.thas20@gmail.com>
2019-12-09 03:31:07 +0100

    'in-user-profile-programme-tab-added-and-changed-some-config-like-access-et-view-hotel'

--- web/modules/custom/projetgr4/src/Controller/ProgrammesController.php
+++ web/modules/custom/projetgr4/src/Controller/ProgrammesController.php
@@ -11,16 +11,49 @@ use Drupal\Core\Url;
 
 class ProgrammesController extends ControllerBase{
 
-    public function content(UserInterface $user)
+    public function programme_status(\Drupal\user\UserInterface $user)
     {
-        
-        $msg = $this->t('you are name @username', ['@username'=> $this->currentUser()->getDisplayName(),]);
-        return ['#markup' => $msg];
-    
-    }
+      $query = \Drupal::database()->select('webform_submission', 's');
+      $query->fields('s', ['sid', 'entity_id'])
+        ->condition('s.uid', $user->id());
+      $sid_entity_id = $query->execute();
 
-    public function programme_status(UserInterface $user)
-    {
-       
+      if(!empty($sid_entity_id)){
+        $user_programme_status = [];
+        foreach($sid_entity_id as $info_ids){
+
+        $query = \Drupal::database()->select('webform_submission', 'w');
+        $query->join('webform_submission_data', 'd', "d.sid = $info_ids->sid AND d.name = 'status'");
+        $query->join('node_field_data', 'n', "n.nid = $info_ids->entity_id");
+        $result = $query
+          ->fields('d', array('value'))
+          ->fields('n', array('title'))
+          ->execute();
+          foreach ($result as $record){
+            $user_programme_status[]= [$record->title, $record->value,];
+          }
+          $status_table = [
+            '#type' => 'table',
+            '#header' => ['Programme Title', 'Status'],
+            '#rows' => $user_programme_status,
+            '#empty'=> $this->t('You are not register any conference yet'),
+          ];
+          return [$status_table];
+        }
+      }
+        $no_data_return_table = [
+        '#type' => 'table',
+        '#header' => ['Programme Title', 'Status'],
+        '#rows' => [
+          'data' => [
+            [
+              'colspan' => 2,
+              'data' => 'You are not register any Programme Yet',
+              'style' => 'background-color: pink; text-align: center'
+            ]
+          ],
+        ],
+      ];
+      return [$no_data_return_table];
     }
 }
